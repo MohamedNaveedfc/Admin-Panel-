d794f9bab4b30fae379e891f18c66c213ae4a60a
Mohamed Naveed <87637249+MohamedNaveedfc@users.noreply.github.com>
2023-01-04 07:56:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include('includes/navbar.php');
   <!-- Content Row -->
   <div class="row">
 
-    <!-- Earnings (Monthly) Card Example -->
+    <!-- Total Registered Admin -->
     <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
@@ -48,14 +48,25 @@ include('includes/navbar.php');
     </div>
 </div>
 
-    <!-- Earnings (Monthly) Card Example -->
+    <!-- Total Registered Foods -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-success shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Registered Foods</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+              <?php
+                            require 'dbconfig.php';
+
+                            $query = "SELECT id FROM food ORDER BY id";  
+                            $query_run = mysqli_query($connection, $query);
+
+                            $row = mysqli_num_rows($query_run);
+                            
+                            echo '<h4> Total Foods: '.$row.'</h4>';
+                        ?>
+              </div>
             </div>
             <div class="col-auto">
               <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
